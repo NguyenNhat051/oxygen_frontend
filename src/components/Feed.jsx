2c09922312d5d43b8bbcdf1766393ab6fecc5068
NguyenNhat <nguyennhat669903@gmail.com>
2022-10-28 01:46:20 +0700

    shufferFeed:   src/components/Feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -11,6 +11,24 @@ const Feed = () => {
   const [loading, setLoading] = useState(false);
   const { categoryId } = useParams();
 
+  function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
   useEffect(() => {
     if (categoryId) {
       setLoading(true);
@@ -22,7 +40,7 @@ const Feed = () => {
     } else {
       setLoading(true);
       client.fetch(feedQuery).then((data) => {
-        setPosts(data);
+        setPosts(shuffle(data));
         setLoading(false);
       });
     }
