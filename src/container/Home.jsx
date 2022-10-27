955170c70e3de9df5f8714880e77f89cff1e09ef
NguyenNhat <nguyennhat669903@gmail.com>
2022-10-28 02:10:44 +0700

    fix reponsive:   src/container/Home.jsx

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -49,7 +49,7 @@ const Home = () => {
                 </div>
 
                 {toggleSidebar && (
-                    <div className="fixed w-1/3 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+                    <div className="fixed min-w-min bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
                         <div className="absolute w-full flex justify-end items-center p-2">
                             <AiFillCloseCircle fontSize={30} className="cursor-pointer dark:text-slate-50" onClick={() => setToggleSidebar(false)} />
                         </div>
