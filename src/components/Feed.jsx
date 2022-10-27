939bf7ec59f6540a85964fd32293d198f9bd9f15
NguyenNhat <nguyennhat669903@gmail.com>
2022-10-28 02:15:23 +0700

    modified:   .gitignore 	modified:   src/components/Feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -15,7 +15,7 @@ const Feed = () => {
     let currentIndex = array.length,  randomIndex;
   
     // While there remain elements to shuffle.
-    while (currentIndex != 0) {
+    while (currentIndex !== 0) {
   
       // Pick a remaining element.
       randomIndex = Math.floor(Math.random() * currentIndex);
