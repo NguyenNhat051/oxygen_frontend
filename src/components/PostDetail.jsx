e676a2117eb93fae2729c3a53266cd6a277ca92d
NguyenNhat <nguyennhat669903@gmail.com>
2022-10-28 16:20:41 +0700

    modified:   src/components/PostDetail.jsx 	modified:   src/index.js

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { MdDownloadForOffline, MdOutlineLink } from 'react-icons/md';
 import { Link, useParams } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
-
 import { client, urlFor } from '../client';
 import MasonryLayout from './MasonryLayout';
 import { postDetailMorePostQuery, postDetailQuery } from '../utils/data';
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
-
 import App from "./App";
 import "./index.css";
-
 import { transitions, positions, Provider as AlertProvider } from "react-alert";
 
 const login = ()=>{
